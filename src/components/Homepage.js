4ae3379f8e4b93c5602acafc5c38c3f4b0fd942b
gnahc2305 <achangmurillo@gmail.com>
2022-11-13 16:54:20 -0500

    create shop container

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,10 @@
+
+
 function Homepage() {
     return (
-      <div className="App">
-        Homepage
+      <div className="home-container">
+        <h1>Amazing Products</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati nesciunt dignissimos nam cumque voluptas, laboriosam nulla quisquam delectus veritatis beatae vitae sint modi esse hic dolores tenetur, totam alias voluptatem.</p>
       </div>
     );
   }
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,14 @@
 function Shop() {
     return (
-      <div className="App">
-        Shop
+      <div>
+        <div className="shop">
+          <h1>Shop</h1>
+
+          <div className="shop-container">
+            <div className="card">
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
